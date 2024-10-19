a503ae5d85f3c19ccc28b4367113b47fcef39f0c
Lysenko <lysenkoden@gmail.com>
2024-10-19 08:19:19 +0200

    completed sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,44 @@
+import { Sidebar } from 'flowbite-react';
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const DashSidebar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <div className="w-full">
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={'User'}
+              labelColor="dark"
+              // my approach to avoid warning about nested link:
+              onClick={() => navigate('/dashboard?tab=profile')}
+              className="cursor-pointer"
+            >
+              Profile
+            </Sidebar.Item>
+          </div>
+
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;

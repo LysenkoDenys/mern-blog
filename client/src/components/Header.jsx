c648cf3aa85be1a48b8aaa124c9f886a7aabc89b
Lysenko <lysenkoden@gmail.com>
2024-10-22 22:45:36 +0200

    added signout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,30 @@ import { CiSearch } from 'react-icons/ci';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 const Header = () => {
   const path = useLocation().pathname;
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message); //
+    }
+  };
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -60,7 +78,7 @@ const Header = () => {
               </Link>
             </Dropdown.Header>
             <Dropdown.Divider />
-            <Dropdown.Item>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="sign-in">

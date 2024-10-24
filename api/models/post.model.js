caa2d8540e190dc437b8950d5532eebdaa78687f
Lysenko <lysenkoden@gmail.com>
2024-10-24 23:05:35 +0200

    added create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://www.webnode.com/blog/wp-content/uploads/2019/04/blog2.png',
+    },
+    category: {
+      type: String,
+      default: 'Uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true } // to fix time when user CRUD posts
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;

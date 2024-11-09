41e7b53e606f80b6f0d5f8cc5a0e3df5b0460943
Lysenko <lysenkoden@gmail.com>
2024-11-09 08:04:26 +0100

    extended quill redactor

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,6 +21,32 @@ const CreatePost = () => {
   const [publishError, setPublishError] = useState(null);
   const navigate = useNavigate();
 
+  //===========================================
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video', 'formula'],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+    [{ direction: 'rtl' }], // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ['clean'], // remove formatting button
+  ];
+  const module = {
+    toolbar: toolbarOptions,
+  };
+  //===========================================
+
   const handlerUploadImage = async () => {
     try {
       if (!file) {
@@ -148,6 +174,7 @@ const CreatePost = () => {
           placeholder="Write something smart, impress me..."
           className="h-72 mb-12"
           required
+          modules={module}
           onChange={(value) => {
             setFormData({ ...formData, content: value });
           }}
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -30,6 +30,32 @@ const UpdatePost = () => {
   const { postId } = useParams();
   const { currentUser } = useSelector((state) => state.user);
 
+  //===========================================
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video', 'formula'],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+    [{ direction: 'rtl' }], // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ['clean'], // remove formatting button
+  ];
+  const module = {
+    toolbar: toolbarOptions,
+  };
+  //===========================================
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -189,6 +215,7 @@ const UpdatePost = () => {
           placeholder="Write something smart, impress me..."
           className="h-72 mb-12"
           required
+          modules={module}
           onChange={setContent} // Update content state on change for debounced update
         />
         <Button type="submit" gradientDuoTone="purpleToPink">
